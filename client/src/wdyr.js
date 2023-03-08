8653f714ff22b6abb4a16cff97281195c0506800
ALAMIN JUMA MAGOTI <61662333+Alamin-Juma@users.noreply.github.com>
2023-03-08 11:35:21 +0300

    updates

---
+++ client/src/wdyr.js
@@ -0,0 +1,11 @@
+/// <reference types="@welldone-software/why-did-you-render" />
+import * as React from 'react';
+import * as ReactRedux from 'react-redux';
+import whyDidYouRender from '@welldone-software/why-did-you-render';
+
+if (import.meta.env.DEV) {
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+    trackExtraHooks: [[ReactRedux, 'useSelector']],
+  });
+}
