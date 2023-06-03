daea6ed5c2b63520fb0104cfff63e92d462154c5
ALAMIN JUMA MAGOTI <61662333+Alamin-Juma@users.noreply.github.com>
2023-06-03 17:41:53 +0300

    events categories refactor

--- client/src/common/sharedComponents/EventCategory/EventCategory.jsx
+++ client/src/common/sharedComponents/EventCategory/EventCategory.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import { Button } from '../../sharedComponents'
 
 import './EventCategory.scss'
-const EventCategory = ({eventData, routeToSingleEvent}) => {
-  const  {imageUrl, title, date, company, price } = eventData;
+const EventCategory = ({ eventData, routeToSingleEvent }) => {
+  const { imageUrl, title, date, company, price } = eventData;
   return (
     <div className='category-container' onClick={routeToSingleEvent}>
-      <div
+      <img
         className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
+        src={imageUrl} />
       <div className='category-body-container'>
         <h2>{title}</h2>
         <p>{date}</p>
         <p>{company}</p>
-        <p>{price}</p>
+        <p>ksh {price}</p>
+        <Button classType='primary' color='blue'>Buy Now</Button>
       </div>
+
     </div>
   )
 }
